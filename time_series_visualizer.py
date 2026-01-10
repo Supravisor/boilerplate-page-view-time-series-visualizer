e717b05a8a17c142c1ebb1452d436d262e1de880
Supravisor <153783117+Supravisor@users.noreply.github.com>
2026-01-11 12:36:56 +1300

    Add outlier filtering to data cleaning process
    Filter out outliers from the data based on quantiles.

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -9,7 +9,7 @@ df = pd.read_csv('fcc-forum-pageviews.csv', index_col='date', parse_dates=True)
 
 # Clean data
 df = df.dropna()
-
+df = df[(df['value'] >= df['value'].quantile(0.025)) & (df['value'] <= df['value'].quantile(0.975))]
 
 def draw_line_plot():
     # Draw line plot
